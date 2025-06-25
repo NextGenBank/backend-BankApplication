0c2f08bbfa390677cfd785d667db2eb1858e644f
danapostolache <dana.postolache12@gmail.com>
2025-06-25 12:50:26 +0200

    comments

--- src/main/java/com/nextgenbank/backend/exception/CustomExceptionHandler.java
+++ src/main/java/com/nextgenbank/backend/exception/CustomExceptionHandler.java
@@ -18,6 +18,7 @@ import java.util.Map;
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
+    //equest body validation fails
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
@@ -27,22 +28,27 @@ public class CustomExceptionHandler {
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
+    //invalid arguments are passed
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Map<String, String>> handleIllegalArgument(IllegalArgumentException ex) {
         return ResponseEntity.badRequest().body(Map.of("error", ex.getMessage()));
     }
 
+    //triggered when user doesn't exist/wrong pass
     @ExceptionHandler({UsernameNotFoundException.class, BadCredentialsException.class})
     public ResponseEntity<String> handleAuthErrors(RuntimeException ex) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
     }
 
+    //fallback for all unhandled exceptions
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, String>> handleGenericException(Exception ex) {
         Map<String, String> error = new HashMap<>();
         error.put("error", ex.getMessage());
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    //triggered when business logic violates rules (like "account balance is isufficient")
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<Map<String, String>> handleIllegalState(IllegalStateException ex) {
         return ResponseEntity.badRequest().body(Map.of("error", ex.getMessage()));
